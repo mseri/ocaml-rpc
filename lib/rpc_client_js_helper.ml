48c2df2e78d82e869b7ca83d2927ae75945d458e
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2013-02-10 00:22:22 +0000

    Now some of this stuff actually works

--- lib/rpc_client_js_helper.ml
+++ lib/rpc_client_js_helper.ml
@@ -8,28 +8,37 @@ let keys obj =
 	List.map (Js.to_string) (Array.to_list (Js.to_array arr))
 
 let is_array obj =
-	Js.instanceof obj Js.array_empty
+	let str = Js.Unsafe.call (Js.Unsafe.variable "Object.prototype.toString") obj [||] in
+	Js.to_string str = "[object Array]"
 
 let mlString_constr = Js.Unsafe.variable "MlString"
 let is_string obj =
 	Js.instanceof obj mlString_constr
 
+let nullobj = Json.unsafe_input (Js.string "null")
+let is_null obj = Json.output obj = Js.string "null"
+
 let rec rpc_of_json json =
 	let ty = Js.typeof json in
 	match (Js.to_string ty) with
 		| "object" ->
-			if is_array ty then begin
+			if is_array json then begin
 				let l = Array.to_list (Js.to_array json) in
 				Rpc.Enum (List.map rpc_of_json l)				
 			end else if is_string json then begin
 				Rpc.String (Js.to_string (Js.Unsafe.coerce json))
+			end else if is_null json then begin
+				Rpc.Null
 			end else begin
 				let okeys = keys json in
 				Rpc.Dict (List.map (fun x -> (x, rpc_of_json (Js.Unsafe.get json (Js.string x)))) okeys)
 			end
 		| "boolean" ->
 			Rpc.Bool (Js.to_bool (Obj.magic json))
-		| _ ->
+		| "number" ->
+			let str = Js.Unsafe.meth_call json "toString" [| |] in
+			Rpc.String (Js.to_string str)
+		| _ -> 
 			Firebug.console##log (Js.string (Printf.sprintf "Ack! got %s" (Js.to_string ty)));
 			Rpc.Bool false
 
@@ -39,9 +48,10 @@ let to_string rpc =
 	let rec inner = function 
 		| Rpc.Dict kvs ->
 			let o = Json.unsafe_input (Js.string "{}") in
-			List.iter (fun (x,y) -> Js.Unsafe.set o (Js.string x) (inner y)) kvs
+			List.iter (fun (x,y) -> Js.Unsafe.set o (Js.string x) (inner y)) kvs;
+			o
 		| Rpc.Int x -> Obj.magic (Js.string (Int64.to_string x))
-		| Rpc.Int32 x -> Obj.magic (Js.string (Int32.to_string x))
+		| Rpc.Int32 x -> Obj.magic x
 		| Rpc.Float x -> Obj.magic (Js.string (string_of_float x))
 		| Rpc.String x -> Obj.magic (Js.string x)
 		| Rpc.Bool x -> Obj.magic (if x then Js._true else Js._false)
@@ -51,16 +61,16 @@ let to_string rpc =
 	in Json.output (inner rpc)
 
 let new_id =
-	let count = ref 0L in
-	(fun () -> count := Int64.add 1L !count; !count)
+	let count = ref 0l in
+	(fun () -> count := Int32.add 1l !count; !count)
 
 let string_of_call call =
 	let json = Rpc.Dict [
 		"method", Rpc.String call.Rpc.name;
 		"params", Rpc.Enum call.Rpc.params;
-		"id", Rpc.Int (new_id ());
+		"id", Rpc.Int32 (new_id ());
 	] in
-	to_string json
+	Js.to_string (to_string json)
 
 exception Malformed_method_response of string
 
@@ -79,10 +89,14 @@ let response_of_string str =
 		| Rpc.Dict d ->
 			let result = get "result" d in
 			let error = get "error" d in
-			let (_:int64) = match get "id" d with Rpc.Int i -> i | _ -> raise (Malformed_method_response "id") in
+			let (_:int64) = try match get "id" d with Rpc.Int i -> i | Rpc.String s -> Int64.of_string s with _ -> 
+				Firebug.console##log (Js.string (Printf.sprintf "Weirdness: %s" (Rpc.to_string (get "id" d))));
+				raise (Malformed_method_response "id") in
 			begin match result, error with
 				| v, Rpc.Null    -> Rpc.success v
 				| Rpc.Null, v    -> Rpc.failure v
 				| x,y        -> raise (Malformed_method_response (Printf.sprintf "<result=%s><error=%s>" (Rpc.to_string x) (Rpc.to_string y)))
 			end
-		| rpc -> failwith "Bah"
+		| rpc -> 
+			Firebug.console##log (Js.string (Rpc.to_string rpc));
+			failwith "Bah"
